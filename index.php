4e6b20dd82271d399ede6921b4ba7db25c4a8910
Axel MAMPIONO <axel.mampiono@gmail.com>
2021-02-22 14:32:45 +0100

    ajout caroussel page index

--- index.php
+++ index.php
@@ -9,41 +9,19 @@
         <title>projet GIT</title>
     </head>
         <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+        <div id="carouselExampleSlidesOnly" class="carousel slide " data-ride="carousel" >
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="photo/45Z47-image1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="photo/45Z47-image2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="photo/87-image2.jpg" alt="Third slide">
+    </div>
   </div>
-</nav>        
+</div>
         </div>
         <footer>
         &copy; Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quisquam temporibus suscipit sit in, necessitatibus fuga incidunt amet autem earum, quaerat pariatur explicabo? Voluptatem adipisci pariatur ullam in quo quam!
